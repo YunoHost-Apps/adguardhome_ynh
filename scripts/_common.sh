cef6d805839f0aeb3e60445238d758c5e07c5474
Kay0u <pierre@kayou.io>
2022-01-03 16:25:24 +0100

    Set the ip or the port if needed

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-bcrypt"
+pkg_dependencies="python3-bcrypt python3-yaml"
 
 #=================================================
 # PERSONAL HELPERS
