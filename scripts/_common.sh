2c8322ce7c830f8c4508db92c49b22c5ebb75929
OniriCorpe <oniricorpe@disroot.org>
2023-12-27 06:24:32 +0100

    if the IP is public and the user doesn't want to expose port 53, skip it

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,21 @@
 # PERSONAL HELPERS
 #=================================================
 
+is_public_ip(){
+	local IP
+    IP="$1"
+	if [[ "$IP" =~ ^10.*|^169.*|^172.*|^192.168.* ]] ; then
+		# private ipv4
+        returns false
+	elif [[ "$IP" =~ ^fd*|^fe80:* ]] ; then
+		# private ipv6
+        returns false
+	else
+		# public ip
+        returns true
+	fi
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
