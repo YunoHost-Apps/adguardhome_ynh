b74820b443e02366150e3530b21218f799c13ef8
OniriCorpe <oniricorpe@disroot.org>
2023-12-28 00:05:40 +0100

    shellcheck: ignore SC2005

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,7 +13,9 @@ configure_network_interface_dnsmasq(){
 
     # get the network interface name for IPv4 and IPv6
     # note: echo the IP route command to prevent a crash if the server doesn't have any IPv4/6
+    # shellcheck disable=SC2005
     ipv4_interface=$(echo "$(ip -4 route get 1.2.3.4 2> /dev/null)" | head -n1 | grep -oP '(?<=dev )\w+' || true)
+    # shellcheck disable=SC2005
     ipv6_interface=$(echo "$(ip -6 route get ::1.2.3.4 2> /dev/null)" | head -n1 | grep -oP '(?<=dev )\w+' || true)
 
     if [ -z "$ipv4_interface" ] && [ -z "$ipv6_interface" ]; then
