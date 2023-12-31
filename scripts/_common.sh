a7ddbdeae2ff7f974c0aac04a10b0a090877f8d7
OniriCorpe <oniricorpe@disroot.org>
2023-12-31 01:07:48 +0100

    fix if or

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -76,7 +76,7 @@ process_ips(){
     for i in $(seq "$(echo "$ips" | wc -w)" -1 1); do
             ip=$(echo "$ips" | awk "{print \$$i}")
             # check if the so-called IP really is one
-            if ynh_validate_ip4 --ip_address="$ip" || ynh_validate_ip6 --ip_address="$ip"; then
+            if [[ $(ynh_validate_ip4 --ip_address="$ip") || $(ynh_validate_ip6 --ip_address="$ip") ]] ; then
                 # don't process if the IP is public and the port 53 closed
                 if [ "$(is_public_ip "$ip")" == 0 ] && [ "$open_port_53" == "false" ] ; then
                     exit 1
